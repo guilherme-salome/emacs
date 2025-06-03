c8ce06c6d6fab651a730ccdf52d4490828be7a93
Guilherme Salome <ferreira_pelucio_salome_guilherme@lilly.com>
2025-06-03 14:37:21 -0400

    Update config.el

--- config.el
+++ config.el
@@ -6,7 +6,7 @@
        (load personal-file)))
 
 ;; Prefer encrypted authentication source
-(setq! auth-sources '("~/.authinfo.gpg" "~/.authinfo"))
+(setq! auth-sources '((:source "~/.authinfo.gpg")))
 
 ;; Basic MacOS adjusments
 (set-frame-parameter nil 'fullscreen 'maximized)
