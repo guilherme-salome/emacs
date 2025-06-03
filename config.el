c24a0685f81a4f5358d5af213cb2394ef80b7f54
Guilherme Salome <ferreira_pelucio_salome_guilherme@lilly.com>
2025-06-03 15:45:48 -0400

    Update config.el

--- config.el
+++ config.el
@@ -5,6 +5,9 @@
      (when (file-exists-p personal-file)
        (load personal-file)))
 
+;; Prefer encrypted authentication source
+(setq! auth-sources '("~/.authinfo.gpg" "~/.authinfo"))
+
 ;; Basic MacOS adjusments
 (set-frame-parameter nil 'fullscreen 'maximized)
 (setq! doom-theme 'doom-fairy-floss
